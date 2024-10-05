c60c6dab165373b9817ac30b83a7d03122af6a11
ojasvi004 <ojasvidoye@gmail.com>
2024-10-05 12:47:39 +0530

    solved c/easy/8.c
    solved c/easy/8.c
    psudo coders

--- C/Easy/8.c
+++ C/Easy/8.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-struct person {
+struct person
+{
     int age;
     float height;
 };
 
-int main() {
-    struct peron p1;
+int main()
+{
+    struct person p1;
     p1.age = 25;
     p1.height = 5.9;
     printf("Age: %d, Height: %.1f", p1.age, p1.height);
