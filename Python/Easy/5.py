d537d05c80a5d5ca09445b5c5e4e3b934cd7d540
maitri-dalvi <maitridalvi2004@gmail.com>
2024-10-05 13:04:09 +0530

    python.5
    python/easy/5/pseudocoders

--- Python/Easy/5.py
+++ Python/Easy/5.py
@@ -1,24 +1,27 @@
-prnt("Hello, world!")
-for i in rang(5):
+print("Hello, world!")
+for i in range(5):
     print(i)
 
 
-pritn("Hello, world!")
-for i in rnage(10):
-    print(i)
-my_varaible = 10
+print("Hello, world!")
+for i in range(10):
+    print(i);
+    
+my_variable = 10
+
 print(my_variable)
 
 x = 10
 y = 5
-result = x - y + z 
-if x = 5: 
+result = x - y 
+if x == 5:
     print("x is 5")
 
 if x > 10:
     print("x is greater")
-else if x == 10:
+elif x == 10:
     print("x is equal")
 
-del my_function() 
+def my_function():
+    return 0 
 
